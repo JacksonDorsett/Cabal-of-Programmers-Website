ad5692464894ce02debc867bf1dabb728c6e1b51
Jackson <dorsettj@gmail.com>
2021-05-08 11:34:00 -0700

    fixed menu

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cabal_of_Programmers_Website
+{
+    public class ApplicationContext
+    {
+        
+    }
+}
