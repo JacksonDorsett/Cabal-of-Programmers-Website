6d1267cf63f67b49a87e3c8cef6d61d705a29239
TorNATO-TAO <nathan.waltz@wsu.edu>
2021-05-08 23:07:23 -0700

    Updated a few things, the navbar is really weird, will have to read more docks to figure out what is going on with that

--- obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,70 +13,70 @@ namespace Cabal_of_Programmers_Website.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/home/jackson/Desktop/Coding/C#/Cabal-of-Programmers-Website/_Imports.razor"
+#line 1 "C:\Users\waltz\Documents\CoP Website\Cabal-of-Programmers-Website\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/home/jackson/Desktop/Coding/C#/Cabal-of-Programmers-Website/_Imports.razor"
+#line 2 "C:\Users\waltz\Documents\CoP Website\Cabal-of-Programmers-Website\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/home/jackson/Desktop/Coding/C#/Cabal-of-Programmers-Website/_Imports.razor"
+#line 3 "C:\Users\waltz\Documents\CoP Website\Cabal-of-Programmers-Website\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/home/jackson/Desktop/Coding/C#/Cabal-of-Programmers-Website/_Imports.razor"
+#line 4 "C:\Users\waltz\Documents\CoP Website\Cabal-of-Programmers-Website\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/home/jackson/Desktop/Coding/C#/Cabal-of-Programmers-Website/_Imports.razor"
+#line 5 "C:\Users\waltz\Documents\CoP Website\Cabal-of-Programmers-Website\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/home/jackson/Desktop/Coding/C#/Cabal-of-Programmers-Website/_Imports.razor"
+#line 6 "C:\Users\waltz\Documents\CoP Website\Cabal-of-Programmers-Website\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/home/jackson/Desktop/Coding/C#/Cabal-of-Programmers-Website/_Imports.razor"
+#line 7 "C:\Users\waltz\Documents\CoP Website\Cabal-of-Programmers-Website\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/home/jackson/Desktop/Coding/C#/Cabal-of-Programmers-Website/_Imports.razor"
+#line 8 "C:\Users\waltz\Documents\CoP Website\Cabal-of-Programmers-Website\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/home/jackson/Desktop/Coding/C#/Cabal-of-Programmers-Website/_Imports.razor"
+#line 9 "C:\Users\waltz\Documents\CoP Website\Cabal-of-Programmers-Website\_Imports.razor"
 using Cabal_of_Programmers_Website;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/home/jackson/Desktop/Coding/C#/Cabal-of-Programmers-Website/_Imports.razor"
+#line 10 "C:\Users\waltz\Documents\CoP Website\Cabal-of-Programmers-Website\_Imports.razor"
 using Cabal_of_Programmers_Website.Shared;
 
 #line default
